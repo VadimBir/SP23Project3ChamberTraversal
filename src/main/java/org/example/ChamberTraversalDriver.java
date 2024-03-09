c91fe1cb943932f5d9cdf6fa801e177f34f18799
StVadimBir <128745468+StVadimBir@users.noreply.github.com>
2024-03-09 17:09:36 -0600

    Minor guidance update

--- src/main/java/org/example/ChamberTraversalDriver.java
+++ src/main/java/org/example/ChamberTraversalDriver.java
@@ -8,22 +8,37 @@ public class ChamberTraversalDriver {
 
     public static char[][] readMaze(String fileName){
         // Code here
+        // During week 8, I was showing how to read the file. You want to convert the file lines, into a 2-D array.
+        // Step.1 Read file
+        // Step.2 Get all the rows
+        // Step.3 Split the rows into each character
+        // Step.4 How you want to have each row being an element for an array and each row element to be an array itself
 
     }
 
     public static void Printmaze(char[][] maze){
         // Designated print out function for a maze, optional
+        // What is a 2D array? its an array of arrays, via a for loop you can print a 1D array, how to print an array or arrays?
     }
 
     public static void move(char[][] maze, int row, int column, int count) {
         // Use recursion to move through the maze at a given coordinate and flood the given chamber with the count
+        // Recurdion would create a new function and it allows to move the state.
+        // For this method, you are at given [Y][X], calling the same method with [Y][X+1] would allow to literally be at the point
+        // relative to which you perform what ever is needed
+
+        // Tip: you want to put the int number as a character into the cell (Line below puts the 1 as '1' not the character with id number of 1
+            // maze[row][column] = (char) (count+'0'); // Using % 10 to keep single digits
     }
 
     public static int count(char[][] maze, int[] startPosition){
         int count = 0;
 
         // Code here, the function checks if the maze still has any unvisited chambers and floods them accordingly
-        // finaly return the count of the chambers
+        // Finally return the count of the chambers
+        // Logic:
+            // From the starting point you flood the chamber with the same character, which is flooded recursively
+            // When recursively you flooded all the branches, you want to increment the count and find another space from which to start flooding again
 
         return count;
     }
@@ -45,21 +60,20 @@ public class ChamberTraversalDriver {
 
         // File given at the room of the project, check it if needed
         // Read the file
-        char[][] maze = readMaze("field.txt"); // 5 chambers col 20 rows 10 start 5,3
-        // has a int array for the starting position
+        char[][] maze = readMaze("field.txt"); // 5 chambers number of col 20, number of rows 10
 
         // Have a main scanner object, declare only one scanner object in the main function
 
-        // print out the maze the is being read from the file
+        // print out the maze that is being read from the file
 
         // Ask and read the starting position from the user via validInput function (scanner passed in and maze reference is passed)
+            // Be careful on the input format, single input for coordinates seperated by space
 
         // Run the count function and print out the total number of chambers starting from the starting position
 
         // Print the number of chambers
 
         // print the maze with the chambers flooded
-
 
     }
 
